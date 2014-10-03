8464a01c8faa864dabf2e18183925fde4965be17
pchristopher <pchristopher@iparadigms.com>
2014-10-03 13:53:46 -0700

    Forgot to add this

---
+++ console/helpers.go
@@ -0,0 +1,19 @@
+package console
+
+import (
+	"fmt"
+	"net/url"
+)
+
+const PageWindowLength = 25
+
+func computeDomainPagination(linkPrefix string, dinfos []DomainInfo, windowLength int) []string {
+	r := []string{}
+	if len(dinfos) == 0 {
+		return r
+	}
+	for i := 0; i < len(dinfos); i = i + windowLength {
+		r = append(r, linkPrefix+"/"+dinfos[i].Domain)
+	}
+	return r
+}
