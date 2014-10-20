9f7091357966d656e09fa1bb5ea4ad6e23a2f39e
pchristopher <pchristopher@iparadigms.com>
2014-10-20 12:49:10 -0700

    Remove console/script/coverage.sh

--- console/script/coverage.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-## This code is here to help me remember the (fairly complicated) invocation of cover I use.
-## not this should be run in the walker/console directory, and it will over write the two
-## files test/coverage.{out,html}
-go test github.com/iParadigms/walker/console/test -cover -coverpkg github.com/iParadigms/walker/console -coverprofile=test/coverage.out && \
-go tool cover -html=test/coverage.out -o test/coverage.html
-
